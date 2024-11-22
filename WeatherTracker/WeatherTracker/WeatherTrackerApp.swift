4ae72d5ac7302bdc4a8fd8bd13ada9550cf8b649
Tarquinio Roberto de Melo Teles <tarquinioteles@me.com>
2024-11-22 12:40:39 -0300

    Initial commit. Starting clock at 12:39 Brazil main time (Sao Paulo)

---
+++ WeatherTracker/WeatherTracker/WeatherTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrackerApp.swift
+//  WeatherTracker
+//
+//  Created by Tarquinio Teles on 22/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherTracker/WeatherTrackerTests/WeatherTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrackerTests.swift
+//  WeatherTrackerTests
+//
+//  Created by Tarquinio Teles on 22/11/24.
+//
+
+import Testing
+@testable import WeatherTracker
+
+struct WeatherTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
